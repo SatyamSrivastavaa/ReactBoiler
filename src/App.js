06385e0ea72cb12d24fea5bf433b5eb6d39adb5c
Satyam <satyam.srivastav@sapient.com>
2020-06-10 13:02:33 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>Your App is Working</div>
+    )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// styles
+import './index.css'
+
+
+// components
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
